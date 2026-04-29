83ba91c028f249d07a161cb1e948ed3b37d8acfd
Argon <smmomm@126.com>
2026-04-29 19:27:07 +0800

    第一个版本

---
+++ .vitepress/config.mjs
@@ -0,0 +1,46 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "RE:CodemaoAPI",
+  description: "下一个由社区维护的编程猫API文档",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "API", link: "/api_main" },
+      { text: "关于", link: "/about" },
+    ],
+
+    sidebar: [
+      {
+        text: "社区功能",
+        items: [
+          { text: "不知道写啥", link: "/api_main" },
+          { text: "不知道写啥", link: "/api_main" },
+        ],
+      },
+    ],
+
+    socialLinks: [
+      { icon: "github", link: "https://github.com/mifongjvav/RE-CodemaoAPI" },
+    ],
+    footer: {
+      message: "基于 MIT 许可发布",
+      copyright: "Copyright © 2026 mifongjvav",
+    },
+
+    editLink: {
+      pattern: "https://github.com/mifongjvav/RE-CodemaoAPI/edit/main/docs/:path",
+      text: "在 GitHub 上编辑此页",
+    },
+
+    lastUpdated: {
+      text: "最后更新于",
+      formatOptions: {
+        dateStyle: "short",
+        timeStyle: "short",
+      },
+    },
+  },
+});
---
+++ index.md
@@ -0,0 +1,25 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "RE:CodemaoAPI"
+  text: "下一代由社区维护的编程猫API文档"
+  tagline: 为了你，为了我们
+  actions:
+    - theme: brand
+      text: 开始吧
+      link: /api_main
+    - theme: alt
+      text: 关于我们
+      link: /about
+
+features:
+  - title: 保持同步
+    details: 得益于庞大的社区，此文档会很快更新最新的API
+  - title: 不知道写啥
+    details: 不知道写啥
+  - title: 不知道写啥
+    details: 不知道写啥
+---
+
